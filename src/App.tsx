b38482e7c846646eb563fcd640fa79669cfa5468
sam <sjglover@udel.edu>
2023-03-13 23:06:39 -0400

    Completed CheckAnswer

--- src/App.tsx
+++ src/App.tsx
@@ -23,9 +23,6 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <hr></hr>
-<<<<<<< HEAD
-            {<DoubleHalf></DoubleHalf>}
-=======
             <CheckAnswer expectedAnswer="42"></CheckAnswer>
             <hr></hr>
             <GiveAttempts></GiveAttempts>
@@ -39,8 +36,7 @@ function App(): JSX.Element {
                 expectedAnswer="b"
             ></MultipleChoiceQuestion>
             <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
->>>>>>> upstream/task-forms
+            {<DoubleHalf></DoubleHalf>}
             <hr></hr>
             <ChooseTeam></ChooseTeam>
             <hr></hr>
--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,27 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [userAnswer, setUserAnswer] = useState<string>("");
+    function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        setUserAnswer(event.target.value);
+    }
     return (
         <div>
             <h3>Check Answer</h3>
+            <Form.Group controlId="formUseAnswer">
+                <Form.Label>Short Answer</Form.Label>
+                <Form.Control
+                    type="text"
+                    value={userAnswer}
+                    onChange={updateAnswer}
+                />
+            </Form.Group>
+            {userAnswer === expectedAnswer ? <span>✔️</span> : <span>❌</span>}
         </div>
     );
 }
