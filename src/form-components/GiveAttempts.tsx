f13fad76b626065fb1c2eae732ebfa8872bbd6c5
sam <sjglover@udel.edu>
2023-03-14 13:15:11 -0400

    Finished MultipleChoiceQuestion

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,27 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [numLeft, setNumLeft] = useState<number>(3);
+    const [numRequest, setNumRequest] = useState<string>("");
+    const newAttempts = parseInt(numRequest) + numLeft;
     return (
         <div>
             <h3>Give Attempts</h3>
+            Number of Attempts Left: {numLeft}
+            <Form.Group controlId="formGiveAttempts">
+                <Form.Label>Requested Number of Attempts: </Form.Label>
+                <Form.Control
+                    type="number"
+                    value={numRequest}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setNumRequest(event.target.value)
+                    }
+                />
+            </Form.Group>
+            {newAttempts}
+            <Button onClick={() => setNumLeft(numLeft - 1)}>Use</Button>
+            <Button onClick={() => setNumLeft(numLeft + 1)}>Gain</Button>
         </div>
     );
 }
